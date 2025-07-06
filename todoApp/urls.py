49868f5a62774e392aaa350ce0e270b403191106
unix-explorer <mwanzaandrew343@gmail.com>
2025-07-06 03:08:24 +0200

    Delete Feature Completed.

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -8,4 +8,5 @@ urlpatterns=[
     path('tasks/',views.taskListView,name='task_list'),
     path('complete/<int:task_id>/',views.taskListView,name='task_complete'),
     path('update/<int:task_id>/',views.taskUpdateView,name='task_update'),
+    path('delete/<int:task_id>/',views.taskDeleteView,name='task_delete'),
 ]
