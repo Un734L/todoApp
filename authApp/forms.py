aea850bc787be3cc4b4b285e11c8dd89c8c2b085
George malindi <manassehmali2005@gmail.com>
2025-07-24 19:30:58 +0200

    added class attributes to forms for styling

--- authApp/forms.py
+++ authApp/forms.py
@@ -2,22 +2,43 @@ from django import forms
 from django.contrib.auth.models import User
 
 class RegisterForm(forms.ModelForm):
-    password=forms.CharField(widget=forms.PasswordInput)
-    passwordConfirm=forms.CharField(widget=forms.PasswordInput,label='Confirm Password:')
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'Password'
+        })
+    )
+    passwordConfirm = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'Confirm Password'
+        }),
+        label='Confirm Password:'
+    )
+
     class Meta:
-        model =User 
-        fields = [
-            'username',
-            'email',
-            'password',
-        ]
+        model = User
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'Username'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'Email'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'Password'
+            }),
+        }
+
     def clean(self):
-        cleaned_data=super().clean()
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        passwordConfirm = cleaned_data.get('passwordConfirm')
 
-        password=cleaned_data.get('password')
-        passwordConfirm=cleaned_data.get('passwordConfirm')
-
-        if password!=passwordConfirm:
+        if password != passwordConfirm:
             raise forms.ValidationError('Passwords do not match.')
         return cleaned_data
-
